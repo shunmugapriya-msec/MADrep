d018474119ab39d1be913a664920eec643ff1916
shunmugapriya-msec <152805192+shunmugapriya-msec@users.noreply.github.com>
2024-11-06 13:53:16 +0530

    Add files via upload

---
+++ MAD/LoginActivity.java
@@ -0,0 +1,29 @@
+public class LoginActivity extends AppCompatActivity {
+    private EditText etEmail, etPassword;
+    private Button btnLogin;
+    private TextView tvRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        tvRegister = findViewById(R.id.tvRegister);
+
+        btnLogin.setOnClickListener(v -> {
+            String email = etEmail.getText().toString();
+            String password = etPassword.getText().toString();
+            // TODO: Implement authentication logic
+            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+            startActivity(intent);
+        });
+
+        tvRegister.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        });
+    }
+}
